6e76b63144e5ac37f81f43c716f55d3b6b24ed8d
Carlos <148070417+ehodu@users.noreply.github.com>
2024-04-17 12:55:03 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
-echo "Script starting..."
-
+echo "Starting script..."
+sleep 3s
 sudo xbps-install -u xbps 
 sudo xbps-install -Syu
 sudo xbps-install pulseaudio dbus xfce4 xorg-minimal xorg-fonts mesa-dri lightdm lightdm-gtk-greeter xfce4-terminal xfce4-plugins NetworkManager network-manager-applet
@@ -8,5 +8,6 @@ sudo ln -s /etc/sv/dbus /var/service
 sudo ln -s /etc/sv/NetworkManager /var/service
 sudo ln -s /etc/sv/dbus /var/service
 sudo ln -s /etc/sv/lightdm /var/service
-echo "Script finished"
+echo "Script finished. Rebooting system..."
+sleep 2s
 sudo reboot
