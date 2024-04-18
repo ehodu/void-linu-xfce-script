a73f052a6e40d38540ffa3d3a6352ea674604f16
Carlos <148070417+ehodu@users.noreply.github.com>
2024-04-17 21:06:02 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 echo "Starting script..."
 sleep 3s
-sudo xbps-install -u xbps 
+sudo xbps-install -yu xbps 
 sudo xbps-install -Syu
-sudo xbps-install pulseaudio dbus xfce4 xorg-minimal xorg-fonts mesa-dri lightdm lightdm-gtk-greeter xfce4-terminal xfce4-plugins NetworkManager network-manager-applet
+sudo xbps-install -Sy pulseaudio dbus xfce4 xorg-minimal xorg-fonts mesa-dri lightdm lightdm-gtk-greeter xfce4-terminal xfce4-plugins NetworkManager network-manager-applet
 
 sudo ln -s /etc/sv/dbus /var/service
 sudo ln -s /etc/sv/NetworkManager /var/service
